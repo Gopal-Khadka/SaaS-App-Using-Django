4cded68ddcd6554d00675d85214b75d6aa072793
Gopal Khadka <khadkagopal.mail@gmail.com>
2024-11-21 15:56:40 +0545

    Learn django templates extensions

--- src/cfehome/views.py
+++ src/cfehome/views.py
@@ -2,4 +2,5 @@ from django.shortcuts import render
 
 
 def home_page_view(request):
-    return render(request, "home.html")
+    my_context = {"title": "Home"}
+    return render(request, "home.html", context=my_context)
