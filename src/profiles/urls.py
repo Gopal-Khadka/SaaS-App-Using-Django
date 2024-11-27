d95f2f56f3ffa8d97bbf9020b2debea2e55f4deb
Gopal Khadka <khadkagopal.mail@gmail.com>
2024-11-27 12:05:56 +0545

    Use perms in templates

--- src/profiles/urls.py
+++ src/profiles/urls.py
@@ -1,4 +1,7 @@
 from django.urls import path
 from . import views
 
-urlpatterns = [path("<username>/", views.profile_view)]
+urlpatterns = [
+    path("", views.profile_list_view),
+    path("<username>/", views.profile_detail_view),
+]
--- src/profiles/views.py
+++ src/profiles/views.py
@@ -6,15 +6,17 @@ User = get_user_model()
 
 
 @login_required
-def profile_view(request, username=None, *args, **kwargs):
-    user = request.user
-    # user = User.objects.get(username == username)
-    # permissions are defined in given manner:
-        #<app_label>.add_<model_name>
-        #<app_label>.change<model_name>
-        #<app_label>.view_<model_name>
-        #<app_label>.delete_<model_name>
-    print(user.has_perm("delete_user"))
-    profile_user_obj = get_object_or_404(User, username = username)
-    return HttpResponse(f"Hello there {username}")
+def profile_list_view(request):
+    context = {"object_list": User.objects.filter(is_active=True)}
+    return render(request, "profiles/list.html", context)
 
+
+@login_required
+def profile_detail_view(request, username=None, *args, **kwargs):
+    user = request.user
+    profile_user_obj = get_object_or_404(User, username=username)
+    context = {
+        "object": profile_user_obj,
+        "owner": profile_user_obj.username == user.username,
+    }
+    return render(request, "profiles/detail.html", context)
