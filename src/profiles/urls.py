5f829db3a42296d23c485d81d43d6ea339b1f584
Amit Neupane <amit-neupane@gmail.com>
2024-12-09 13:29:55 +0545

    Decouple dashboard views

--- src/profiles/urls.py
+++ src/profiles/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 urlpatterns = [
     path("", views.profile_list_view),
-    path("<username>/", views.profile_detail_view),
+    path("<username>/", views.profile_detail_view,name="profile_detail_view"),
 ]
