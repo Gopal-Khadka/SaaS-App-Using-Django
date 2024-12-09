dcf65e9b33c567348a7e27782c395793e5a10431
Amit Neupane <amit-neupane@gmail.com>
2024-12-09 12:48:24 +0545

    Add dashboard view for auth users

--- src/landing/views.py
+++ src/landing/views.py
@@ -2,9 +2,12 @@ import helpers.numbers
 from django.shortcuts import render
 
 from visits.models import PageVisits
+from dashboard.views import dashboard_view
 
 
 def landing_page_view(request):
+    if request.user.is_authenticated:
+        return dashboard_view(request)
     qs = PageVisits.objects.all()
     count = qs.count()
     page_views_count = helpers.numbers.shorten_number(count * 100_000)
